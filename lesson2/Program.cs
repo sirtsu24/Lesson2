367039b4fef81a6ab482442bc61d619245725200
Opilane TTHK <opilane@tthk.ee>
2019-09-13 17:13:32 +0300

    String interpolation

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -9,6 +9,23 @@ namespace lesson2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            string firstName;
+            string lastName;
+            string favoriteColor;
+            Console.WriteLine("Enter your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your lastname:");
+            lastName = Console.ReadLine();
+            //Console.WriteLine("Nice to meet you, " + firstName + " " + lastName + "!");
+            //I'm just a comment
+           // Console.WriteLine("Nice to meet you {0} {1}", firstName, lastName);
+            
+            Console.WriteLine("Whats your favorite color?");
+            favoriteColor = Console.ReadLine();
+            Console.WriteLine("Your favorite color is {0}", color);
+            Console.WriteLine("Hello, {0} {1}. Your favorite color is {2}", firstName, lastName, favoriteColor);
+            Console.ReadLine();
         }
     }
 }
